518d27041a2eedbb82ed346cbff4c84d5bc76db7
mild0wl <saikrishna106@yahoo.com>
2022-02-05 22:25:41 +0530

    Update s3-recon-from-subs.py

--- s3recon/s3-recon-from-subs.py
+++ s3recon/s3-recon-from-subs.py
@@ -4,6 +4,9 @@ import sys
 # or we can use 
 # httpx -l s3-urls.txt -mc 200 -silent -o s3-alive.txt -t 200
 # s3-url eg: https://s3.amazonaws.com/subdomain/
+
+# todo
+## add concurrency 
 with open(sys.argv[1]) as sf:
     subs = sf.readlines()
     for sub in subs:
