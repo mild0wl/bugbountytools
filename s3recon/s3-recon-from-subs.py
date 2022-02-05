883bfd3e4c0d0bb7a3e06b2411893f93a97fc420
mild0wl <saikrishna106@yahoo.com>
2022-02-05 22:24:51 +0530

    Update s3-recon-from-subs.py

--- s3recon/s3-recon-from-subs.py
+++ s3recon/s3-recon-from-subs.py
@@ -1,7 +1,9 @@
 import requests
 import sys
 
-
+# or we can use 
+# httpx -l s3-urls.txt -mc 200 -silent -o s3-alive.txt -t 200
+# s3-url eg: https://s3.amazonaws.com/subdomain/
 with open(sys.argv[1]) as sf:
     subs = sf.readlines()
     for sub in subs:
